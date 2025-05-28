e6b40398e62299f013b253de375e597f06f3b60d
saezeric <saezescalonaeric@fpllefia.com>
2025-05-28 19:09:27 +0200

    Ajustes con layout, no se estaba viendo todo

---
+++ src/app/Docs/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function Docs() {
+  return (
+    <>
+      <div className="flex flex-col items-center p-[100px] min-h-screen w-full text-white">
+        <div className="w-[672px] h-[156px] flex flex-col justify-center items-center space-y-6">
+          <h1 className="text-center mb-4 font-bold text-[48px]">
+            Bienvenido a Docs
+          </h1>
+        </div>
+        <div className="w-full flex justify-center">
+          <Button className="bg-white text-black hover:bg-gray-200 w-[122px] h-[32px] rounded-[6px]">
+            Get Started
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
